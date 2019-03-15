9e91405dd2bef939b2a501d787650d58b905cbb3
lmy2013 <40338169+lmy2013@users.noreply.github.com>
2019-03-15 18:17:05 +0800

    Update subtitles.py
    优化了一小下,btw,使用的时候要注意字幕文件的编码,如果运行失败可以把编码改成utf-8试试

--- subtitles.py
+++ subtitles.py
@@ -1,8 +1,8 @@
 import re
 import openpyxl
 
-# 读入字幕文件,注意要是srt类型
-text = open('The.Big.Bang.Theory.S11E09.720p.HDTV.x264-AVS.chs.eng.简体&英文.srt', 'r')
+# 读入字幕文件
+text = open('10.srt', 'r')
 
 # 空行、行数标号正则表达式
 rgx_none_and_num = re.compile(r'\d{1,2}\n')
@@ -14,18 +14,15 @@ rgx_time = re.compile(r'\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d\n')
 first_step = text.readlines()
 
 # 新建一个字幕文件
-new_file = open('C:\\Users\\18506\\Desktop\\subtitles.srt', 'w')
+new_file = open('C:\\Users\\18506\\Desktop\\10.srt', 'w')
 
-# 建立4000后的字典,可以根据个人需要更改,Sheet1,(row,2)等参数根据实际需求填写
+# 建立4000后的字典
 wordlist4001 = {}
 excel_content = openpyxl.load_workbook('COCA20000增强版.xlsx')
 sheet = excel_content['Sheet1']
-for row in range(4000, 20201):
+for row in range(4003, 20201):
     wordlist4001[sheet.cell(row, 2).value] = sheet.cell(row, 6).value
-# 建立4000前的字典
-wordlist4000 = {}
-for row in range(2, 4002):
-    wordlist4000[sheet.cell(row, 2).value] = sheet.cell(row, 6).value
+
 # 挑选出文字行进行处理
 for line in first_step:
 
@@ -46,10 +43,7 @@ for line in first_step:
         for word in words:
 
             # 如果单词不在字典中，则跳过
-            if word in wordlist4000:
-                pass
-            # 如果单词在字典中，则添加翻译
-            elif word not in wordlist4001:
+            if word not in wordlist4001:
                 pass
             else:
                 new_word = word + ':' + str(wordlist4001[word]) + '\n'
